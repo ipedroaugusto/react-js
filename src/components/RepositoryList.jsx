eca7eb66d2d9c406fd560ae733e7ab36951685c4
Pedro Augusto <79206426+ipedroaugusto@users.noreply.github.com>
2021-09-04 17:23:35 -0300

    primeiro commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+if( navigator.userAgent.toLowerCase().indexOf('chrome') > -1){
+    // Google chrome
+ }
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }, []);
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                {repositories.map(repository => {  //percorre o repositories e retorna um repository ntem para cada repository
+                    return <RepositoryItem repository={repository} />
+                })}
+            </ul>
+        </section>
+    );
+}
